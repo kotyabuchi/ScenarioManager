338f766793a82a3818d3be2a84e55c7d69298097
kotyabuchi <kotyabuchi@gmail.com>
2024-07-05 10:42:02 +0900

    シナリオのカードとスケルトンのレイアウトを少し変更

--- src/app/dashboard/scenarios/ui/ScenarioListSkeleton.tsx
+++ src/app/dashboard/scenarios/ui/ScenarioListSkeleton.tsx
@@ -2,15 +2,15 @@ import { Skeleton } from "@nextui-org/react";
 
 function CardSkeleton() {
   return (
-    <div className="flex flex-row gap-3 w-full h-full p-4 shadow-soft-sm rounded-2xl bg-white duration-150 items-center">
+    <div className="flex flex-row gap-3 w-full h-full p-4 rounded-2xl bg-white items-center">
       <Skeleton className="shrink-0 rounded-lg w-24 h-24 md:w-28 md:h-28" />
       <div className="flex flex-col gap-1 w-fit">
         <div className='flex gap-2'>
-          <Skeleton className="rounded-full h-5 w-32 px-2" />
-          <Skeleton className="rounded-full h-5 w-24 px-2" />
+          <Skeleton className="rounded-full h-5 max-w-[30%] w-32 px-2" />
+          <Skeleton className="rounded-full h-5 max-w-[20%] w-24 px-2" />
         </div>
-        <div className="flex flex-row flex-wrap gap-y-0 gap-3 items-baseline">
-          <Skeleton className="rounded-full h-6 w-60" />
+        <div className="flex flex-col lg:flex-row flex-wrap gap-y-1 gap-3 items-baseline">
+          <Skeleton className="rounded-full h-6 md:w-52 lg:w-60" />
           <Skeleton className="rounded-full h-4 w-16" />
         </div>
         <div className="flex flex-wrap gap-x-2 gap-y-1 text-xs">
@@ -19,9 +19,9 @@ function CardSkeleton() {
           <Skeleton className="rounded-full h-6 w-16 bg-success-100" />
         </div>
         <div className="flex flex-col gap-[2px]">
-          <Skeleton className="rounded-full h-4 w-96" />
-          <Skeleton className="rounded-full h-4 w-64" />
-          <Skeleton className="rounded-full h-4 w-80" />
+          <Skeleton className="rounded-full h-4 max-w-[55%] md:max-w-[60%] lg:max-w-[100%] w-96" />
+          <Skeleton className="rounded-full h-4 max-w-[30%] md:max-w-[40%] lg:max-w-[70%] w-64" />
+          <Skeleton className="rounded-full h-4 max-w-[40%] md:max-w-[50%] lg:max-w-[850%] w-80" />
         </div>
       </div>
     </div>
