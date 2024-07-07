f39537b23df543b57285d675e80cf3a4f07ae597
kotyabuchi <kotyabuchi@gmail.com>
2024-07-07 22:14:07 +0900

    シナリオ一覧のカードをlgから2列表示に変更

--- src/app/dashboard/scenarios/ui/ScenarioListClient.tsx
+++ src/app/dashboard/scenarios/ui/ScenarioListClient.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import { Button } from '@nextui-org/react';
-import { useCallback, useState, useTransition } from 'react'
+import { useCallback, useState, useTransition } from 'react';
 import { toast } from 'sonner';
 
 type LoadMoreAction = (
@@ -9,23 +9,26 @@ type LoadMoreAction = (
   offset: number
 ) => Promise<readonly [JSX.Element[], number | undefined]>;
 
-export default function ScenarioListClient(
-  {
-    initialScenarios,
-    query,
-    pageSize,
-    loadMoreAction,
-  }: {
-    initialScenarios: JSX.Element[],
-    query: object | undefined,
-    pageSize: number,
-    loadMoreAction: LoadMoreAction,
-  }) {
+export default function ScenarioListClient({
+  initialScenarios,
+  query,
+  pageSize,
+  loadMoreAction,
+}: {
+  initialScenarios: JSX.Element[];
+  query: object | undefined;
+  pageSize: number;
+  loadMoreAction: LoadMoreAction;
+}) {
   const [scenarios, setScenarios] = useState(initialScenarios);
-  const [offset, setOffset] = useState<number | undefined>(initialScenarios.length);
+  const [offset, setOffset] = useState<number | undefined>(
+    initialScenarios.length
+  );
   const [loading, setLoading] = useState(false);
-  const [allDataLoaded, setAllDataLoaded] = useState(initialScenarios.length < pageSize);
-  const [isPending, startTransition] = useTransition();
+  const [allDataLoaded, setAllDataLoaded] = useState(
+    initialScenarios.length < pageSize
+  );
+  const [_, startTransition] = useTransition();
 
   const loadMore = useCallback(async () => {
     setLoading(true);
@@ -37,41 +40,39 @@ export default function ScenarioListClient(
       }
       const [newScenarios, next] = await loadMoreAction(query, offset);
       startTransition(() => {
-        setScenarios(prev => [...prev, ...newScenarios]);
+        setScenarios((prev) => [...prev, ...newScenarios]);
         setOffset(next);
         if (!next) {
           setAllDataLoaded(true);
-          toast.info("全てのシナリオを読み込みました。");
+          toast.info('全てのシナリオを読み込みました。');
           return;
         }
       });
     } catch (error) {
       console.error(error);
-      toast.error("シナリオの読み込みに失敗しました。");
+      toast.error('シナリオの読み込みに失敗しました。');
     } finally {
       setLoading(false);
     }
   }, [offset, loadMoreAction, query]);
 
   return (
-    <div className="flex flex-col gap-3">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full h-full">
+    <div className='flex flex-col gap-3'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 w-full h-full'>
         {scenarios}
       </div>
-      {
-        !allDataLoaded && (
-          <Button
-            className='w-80 self-center px-12'
-            color='primary'
-            variant='flat'
-            onPress={loadMore}
-            isLoading={loading}
-            disabled={loading}
-          >
-            {loading ? "読み込み中..." : "もっと見る"}
-          </Button>
-        )
-      }
+      {!allDataLoaded && (
+        <Button
+          className='w-80 self-center px-12'
+          color='primary'
+          variant='flat'
+          onPress={loadMore}
+          isLoading={loading}
+          disabled={loading}
+        >
+          {loading ? '読み込み中...' : 'もっと見る'}
+        </Button>
+      )}
     </div>
-  )
+  );
 }
